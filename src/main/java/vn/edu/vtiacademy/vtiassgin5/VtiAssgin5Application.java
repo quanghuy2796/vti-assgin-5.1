35c7fa607ca747c90c5bc633df061c2c9139a0a5
quanghuy2796 <140720173+quanghuy2796@users.noreply.github.com>
2023-08-26 20:45:47 +0700

    init project

---
+++ src/main/java/vn/edu/vtiacademy/vtiassgin5/VtiAssgin5Application.java
@@ -0,0 +1,13 @@
+package vn.edu.vtiacademy.vtiassgin5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtiAssgin5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtiAssgin5Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/vtiacademy/vtiassgin5/VtiAssgin5ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vtiacademy.vtiassgin5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtiAssgin5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
